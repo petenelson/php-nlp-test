e67b2adfac7aabbf22c9b278befe6755ed106100
Pete Nelson <pete@petenelson.com>
2018-10-17 10:41:43 -0500

    basic example counting tokens

---
+++ nlp.php
@@ -0,0 +1,62 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use NlpTools\Tokenizers\WhitespaceTokenizer;
+use NlpTools\Similarity\CosineSimilarity;
+
+if ( count( $argv ) <= 1 ) {
+	echo 'Input filename is required.' . PHP_EOL;
+	return;
+}
+
+$filename = $argv[1];
+
+if ( ! file_exists( $filename ) ) {
+	echo "Input filename {$filename} does not exist." . PHP_EOL;
+	return;
+}
+
+$content = file_get_contents( $filename );
+
+if ( ! empty( $content ) ) {
+	$content = strip_tags( $content );
+}
+
+if ( empty( $content ) ) {
+	echo "Input filename {$content} is empty." . PHP_EOL;
+	return;
+}
+
+$stop_words = explode( PHP_EOL, file_get_contents( 'stop-words.txt' ) );
+$strip_chars = explode( ' ', '. ! , $ \' " ? ; : @ # % & * ( ) - + =');
+
+$tokenizer = new WhitespaceTokenizer();
+$sim = new CosineSimilarity();
+ 
+$results = $tokenizer->tokenize( $content );
+ 
+$tokens = [];
+
+if ( is_array( $results ) ) {
+
+	foreach ( $results as $token ) {
+
+		$token = str_replace( $strip_chars, '', strtolower( $token ) );
+
+		if ( strlen( $token ) >= 3 && ! in_array( $token, $stop_words ) ) {
+
+			if ( ! isset( $tokens[ $token ] ) ) {
+				$tokens[ $token ] = 1;
+			} else {
+				$tokens[ $token ]++;
+			}
+		}
+	}
+}
+
+arsort( $tokens );
+
+$tokens = array_slice( $tokens, 0, 20, true );
+
+print_r( $tokens );
